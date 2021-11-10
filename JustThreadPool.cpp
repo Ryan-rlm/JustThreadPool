e55ff6dd48edb2b79f699bef50ff795bee7bc564
ryan <ryan_rlm@foxmail.com>
2021-11-10 21:00:55 +0800

    pimpl模式，格式调整

--- JustThreadPool.cpp
+++ JustThreadPool.cpp
@@ -1,3 +1,8 @@
+﻿
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
+#include <vector>
 
 #include "JustThreadPool.h"
 using namespace Just;
@@ -12,52 +17,60 @@ namespace
     }
 }
 
+struct ThreadPool::Data
+{
+    moodycamel::BlockingConcurrentQueue<Task> task_queue; // 工作队列
+
+    std::shared_mutex pool_mutex;
+    size_t thread_size;
+    std::vector<std::thread> thread_vec;  // 线程池
+
+    Status stat = Status::Inited;
+    Order order = Order::None;
+};
 
 void ThreadPool::work_func()
 {
     bool got = false;
-    
+
     for (;;)
     {
         got = false;
         Task task;
 
-        got = task_queue.wait_dequeue_timed(task, std::chrono::seconds(1));
+        got = d->task_queue.wait_dequeue_timed(task, std::chrono::seconds(1));
         if (got)
             task();
 
-        if (order == Order::Stop)
+        if (d->order == Order::Stop)
         {
             break;
         }
-        else if (order == Order::StopAndDone)
+        else if (d->order == Order::StopAndDone)
         {
-            if (task_queue.size_approx() == 0)
+            if (d->task_queue.size_approx() == 0)
                 break;
         }
     }
 }
 
-ThreadPool::ThreadPool()
-    : task_queue()
-    , pool_mutex()
-    , thread_size(KERNAL_COUNT)
-    , thread_vec()
-    , stat(Status::Inited)
-    , order(Order::None)
+void ThreadPool::task_enqueue(Task t)
 {
-    start(thread_size);
+    d->task_queue.enqueue(t);
+}
+
+ThreadPool::ThreadPool()
+    : d{ std::make_unique<Data>() }
+{
+    d->thread_size = KERNAL_COUNT;
+    start(d->thread_size);
 }
 
 ThreadPool::ThreadPool(size_t thread_hint)
-    : task_queue()
-    , pool_mutex()
-    , thread_size(usefulThreadHint(thread_hint) ? thread_hint : KERNAL_COUNT)
-    , thread_vec()
-    , stat(Status::Inited)
-    , order(Order::None)
+    : d{ std::make_unique<Data>() }
 {
-    start(thread_size);
+    d->thread_size = usefulThreadHint(thread_hint) ? thread_hint : KERNAL_COUNT;
+    start(d->thread_size);
 }
 
 ThreadPool::~ThreadPool()
@@ -67,67 +80,66 @@ ThreadPool::~ThreadPool()
 
 size_t ThreadPool::thread_count() const
 {
-    return thread_size;
+    return d->thread_size;
 }
 
 size_t ThreadPool::task_count_approx() const
 {
-    return task_queue.size_approx();
+    return d->task_queue.size_approx();
 }
 
 void ThreadPool::clear()
 {
-    std::lock_guard locker(pool_mutex);
-    
+    std::unique_lock<std::shared_mutex> locker(d->pool_mutex);
 }
 
 bool ThreadPool::start(size_t thread_hint/* = 0*/)
 {
-    std::lock_guard locker(pool_mutex);
-    if (stat != Status::Inited
-        && stat != Status::Stoped)
+    std::unique_lock<std::shared_mutex> locker(d->pool_mutex);
+    if (d->stat != Status::Inited
+        && d->stat != Status::Stoped)
         return false;
 
-    stat = Status::Starting;
-    order = Order::None;
-    thread_size = usefulThreadHint(thread_hint) ? thread_hint : KERNAL_COUNT;
-    thread_vec.clear();
+    d->stat = Status::Starting;
+    d->order = Order::None;
+    d->thread_size = usefulThreadHint(thread_hint) ? thread_hint : KERNAL_COUNT;
+    d->thread_vec.clear();
 
-    for (size_t i = 0; i < thread_size; i++)
+    for (size_t i = 0; i < d->thread_size; i++)
     {
-        thread_vec.emplace_back(&ThreadPool::work_func, this);
+        d->thread_vec.emplace_back(&ThreadPool::work_func, this);
     }
 
-    stat = Status::Running;
+    d->stat = Status::Running;
 
     return true;
 }
 
-ThreadPool::Status ThreadPool::status()
+ThreadPool::Status ThreadPool::status() const
 {
-    std::lock_guard locker(pool_mutex);
+    std::shared_lock<std::shared_mutex> locker(d->pool_mutex);
 
-    return stat;
+    return d->stat;
 }
 
 void ThreadPool::stop(Order od)
 {
     if (od == Order::None)
         return;
-    
-    std::lock_guard locker(pool_mutex);
-    stat = Status::Stopping;
-    order = od;
 
-    for (auto& it : thread_vec)
+    std::unique_lock<std::shared_mutex> locker(d->pool_mutex);
+    d->stat = Status::Stopping;
+    d->order = od;
+
+    for (auto& it : d->thread_vec)
     {
         if (it.joinable())
             it.join();
     }
-    
-    thread_vec.clear();
-    stat = Status::Stoped;
-    order = Order::None;
+
+    d->thread_vec.clear();
+    d->stat = Status::Stoped;
+    d->order = Order::None;
 }
 
 ThreadPool* Just::commonThreadPool()
--- JustThreadPool.h
+++ JustThreadPool.h
@@ -1,95 +1,86 @@
-
+﻿
 #pragma once
 #ifndef __JUSTTHREADPOOL_H__
 #define __JUSTTHREADPOOL_H__
 
-#include <mutex>
 #include <future>
-#include <thread>
-#include <vector>
-
+#include <memory>
+#include <functional>
 #include <concurrentqueue/blockingconcurrentqueue.h>
 
 
 namespace Just
 {
-    using Task = std::function<void ()>;
+    using Task = std::function<void()>;
 
     class ThreadPool final
     {
-        public:
-            enum class Status
-            {
-                None,
-                Inited,    // 刚初始化
-                Starting,  // 开始创建线程池
-                Running,   // 正在执行
-                Stopping,  // 正在停止
-                Stoped,    // 已停止
-            };
+    public:
+        enum class Status
+        {
+            None,
+            Inited,    // 刚初始化
+            Starting,  // 开始创建线程池
+            Running,   // 正在执行
+            Stopping,  // 正在停止
+            Stoped,    // 已停止
+        };
 
-            enum class Order
-            {
-                None,
-                Stop,
-                StopAndDone,
-            };
+        enum class Order
+        {
+            None,
+            Stop,
+            StopAndDone,
+        };
 
-        private:
-            moodycamel::BlockingConcurrentQueue<Task> task_queue; // 工作队列
+    private:
+        struct Data;
+        std::unique_ptr<Data> d;
 
-            std::mutex pool_mutex;
-            size_t thread_size;
-            std::vector<std::thread> thread_vec;  // 线程池
+        void work_func();
+        void task_enqueue(Task t);
 
-            Status stat;
-            Order order;
+    public:
+        ThreadPool();
+        ThreadPool(size_t thread_hint);
+        ~ThreadPool();
 
-            void work_func();
-            
-        public:
-            ThreadPool();
-            ThreadPool(size_t thread_hint);
-            ~ThreadPool();
+        size_t thread_count() const;
+        size_t task_count_approx() const;
 
-            size_t thread_count() const;
-            size_t task_count_approx() const;
-
-            template<typename Func, typename... Args>
-            std::future<std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>>
+        template<typename Func, typename... Args>
+        std::future<std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>>
             run(Func&& func, Args&&... args)
-            {
-                using ret_t = typename std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>;
-                auto pkg_task = std::make_shared<std::packaged_task<ret_t(std::decay_t<Args>...)>>(std::forward<Func>(func), std::forward<Args>(args)...);
+        {
+            using ret_t = typename std::result_of_t  <std::decay_t<Func>(std::decay_t<Args>...)>;
+            auto pkg_task = std::make_shared<std::packaged_task<ret_t(std::decay_t<Args>...)>>(std::forward<Func>(func), std::forward<Args>(args)...);
 
-                task_queue.enqueue([pkg_task]() -> void {
-                    (*pkg_task)();
-                });
+            task_enqueue([pkg_task]() { (*pkg_task)(); });
 
-                return pkg_task->get_future();
-            }
+            return pkg_task->get_future();
+        }
 
-            void clear();
+        void clear();
 
-            bool start(size_t thread_hint = 0);
-            Status status();
-            void stop(Order od = Order::StopAndDone);
+        bool start(size_t thread_hint = 0);
+        Status status() const;
+        void stop(Order od = Order::StopAndDone);
     };
 
     ThreadPool* commonThreadPool();
 
     template<typename Func, typename... Args>
-    std::future<std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>>
-    async(ThreadPool& threadPool, Func&& func, Args&&... args)
+    std::future<std::result_of_t  <std::decay_t<Func>(std::decay_t<Args>...)>>
+        async(ThreadPool& threadPool, Func&& func, Args&&... args)
     {
-      return threadPool.run(std::forward<Func>(func), std::forward<Args>(args)...);
+        return threadPool.run(std::forward<Func>(func), std::forward<Args>(args)...);
     }
 
     template<typename Func, typename... Args>
-    std::future<std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>>
-    async(Func&& func, Args&&... args)
+    std::future<std::result_of_t  <std::decay_t<Func>(std::decay_t<Args>...)>>
+        async(Func&& func, Args&&... args)
     {
-      return commonThreadPool()->run(std::forward<Func>(func), std::forward<Args>(args)...);
+        return commonThreadPool()->run(std::forward<Func>(func), std::forward<Args>(args)...);
     }
 }
 
